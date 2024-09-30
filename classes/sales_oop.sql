c3953fe0f28b5d3d7364d44a2d31e7b53958541e
lord-kiana <kianaolemberio2001@gmail.com>
2024-09-30 10:02:35 +0800

    real sql yawa

--- classes/sales_oop.sql
+++ classes/sales_oop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 30, 2024 at 03:57 AM
+-- Generation Time: Sep 30, 2024 at 04:02 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,33 @@ SET time_zone = "+00:00";
 --
 -- Database: `sales_oop`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `order_date` date DEFAULT NULL,
+  `total_cost` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_items`
+--
+
+CREATE TABLE `order_items` (
+  `order_item_id` int(11) NOT NULL,
+  `order_id` int(11) DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -120,9 +147,60 @@ INSERT INTO `products` (`product_id`, `product_name`, `price`, `quantity`) VALUE
 (79, 'Shrimp', 6.00, 4),
 (80, 'Tuna Canned', 35.00, 5);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(50) DEFAULT 'cashier'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `username`, `password`, `role`) VALUES
+(1, 'Kiana', 'Olemberio', 'admin', '$2y$10$K46opVHBfe1O.tp1waLiPuLQlEcuV5nwcGtPjaaGb8weDc1TLNr1W', 'admin'),
+(2, 'sheng', 'sheng', 'sheng', '$2y$10$NDRy9TanSMukGaUsR11Vhu0Aepc0c43nASAr1L47Q.36OVfZOVNXO', 'admin'),
+(3, 'sheng', 'sheng', 'sheng1', '$2y$10$jz7YBV66tGFEsq02Je316Oe5EbItRqcpALEzHnDUqPsBEykvEb6DG', 'admin'),
+(4, 'sheng', 'petalcurin', 'sheeeeyng123', '$2y$10$QpcdAJd055BRr8HA0Vy0jeCH3wdJuiHVyOZIiCsrwbWd9hgD2kZqe', 'cashier'),
+(5, 'Kiana', 'Olemberio', '12345', '$2y$10$JXvQ/kJh.X1X6Uc36iZJAuG1JuspmzGYewGTOPNFbHF5c7zhLbMtK', 'cashier'),
+(6, 'test', 'test', 'test', '$2y$10$cjT3XZZBpuf4JXUtzAzc2Ou9xfeQQj59210LsnP4OngJAdJAqrsYy', 'cashier'),
+(7, 'kiana', 'kiana', 'admin2', '$2y$10$AbYlPfih/78tHO0JRrZssekAqxOgpPvzjxRAeIcTboC5KeqKnUQ7a', 'cashier'),
+(8, 'test3', 'test3', 'test3', '$2y$10$zP85FheLED0HncZvMCe4kuU5Z8cdQUZT.Dlbyi3R7EwoyXmyg2q.W', 'cashier'),
+(9, 'test4', 'test4', 'test4', '$2y$10$yddie7zwOKAcP6ixiRwdAO.P6ePWZ81KP7xGdL9iNNw5K90VFk4ve', 'cashier'),
+(10, 'she', 'she', 'she', '$2y$10$Z4lvTbkMRwUKxcUl61K24emcoU0pewM0G1SCjk4gx7u7Hjt1eRpWW', 'cashier'),
+(11, 'william', 'jabasa', 'will', '$2y$10$mFtgsyfn6Q9A4z.mBKtFi.nS1AN.oWK3Z5W.DA33PWN2xzXjmrx.2', 'cashier'),
+(12, 'jiselle', 'jiselle', 'jiselle', '$2y$10$fx0jnoS7znFTnKtws0vZTucC0ThlCOkxu9KR5CF4pDVg/HDmpWhhq', 'cashier'),
+(13, 'shan', 'shan', 'shan', '$2y$10$G2gfdwo7HKeBhVQOa8zeh.4vla6y0bj.9eaFFGKc3O7iTjM.Lwwre', 'cashier'),
+(14, 'shan', 'shan', 'shan2', '$2y$10$VdwHaUBeqgd86RFUWYWNoOhpLa3QDtR4wmrLgoxU0lf9JEKq.eA4i', 'cashier'),
+(15, 'test5', 'test5', 'test5', '$2y$10$BhN4dSnLjGQ1G0mAm8weeOtx5yZflhcwrL2F5mYmQbBkOqlnFzKiy', 'cashier');
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD PRIMARY KEY (`order_item_id`),
+  ADD KEY `order_id` (`order_id`),
+  ADD KEY `product_id` (`product_id`);
 
 --
 -- Indexes for table `products`
@@ -131,14 +209,55 @@ ALTER TABLE `products`
   ADD PRIMARY KEY (`product_id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
+
+--
+-- AUTO_INCREMENT for table `order_items`
+--
+ALTER TABLE `order_items`
+  MODIFY `order_item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
   MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=107;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`),
+  ADD CONSTRAINT `order_items_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
